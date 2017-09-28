4f39cbbf5768fb23d1b8e3d9d180b8a5db1460d5
Hari Krishna Bista <harikrishna@apple.com>
2017-09-27 23:15:27 -0700

    first commit

---
+++ NewsFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsFeed
+//
+//  Created by Bista on 9/27/17.
+//  Copyright © 2017 practice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewsFeed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewsFeed
+//
+//  Created by Bista on 9/27/17.
+//  Copyright © 2017 practice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
